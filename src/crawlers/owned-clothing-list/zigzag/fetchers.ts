663c973a22685f353bd43cdc6ddb4b1e7aedee88
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-10-19 14:35:57 +0900

    :art: refactor: integrate extractors to fetchers
    - integrate extractor functions to fetchers

--- src/crawlers/owned-clothing-list/zigzag/fetchers.ts
+++ src/crawlers/owned-clothing-list/zigzag/fetchers.ts
@@ -167,6 +167,26 @@ const fetchZigZagOrderList = (
   });
 };
 
+const extractClothingsInZigZagOrderList = (
+  zigZagOrderList: ZigZagOrderListType
+): IClothing[] => {
+  let clothings: IClothing[] = [];
+  for (let order of zigZagOrderList) {
+    for (let orderShop of order.order_shop_list) {
+      for (let orderItem of orderShop.order_item_list) {
+        let productInfo = orderItem.product_info;
+        clothings.push({
+          productUrl: productInfo.url,
+          imageUrl: productInfo.image_url,
+          options: productInfo.options,
+        });
+      }
+    }
+  }
+
+  return clothings;
+};
+
 const fetchZigZagOwnedClothingListWithCookie = async (cookie: string) => {
   const orderIdList: ZigZagOrderIdListType = await fetchZigZagOrderIdList(
     cookie
